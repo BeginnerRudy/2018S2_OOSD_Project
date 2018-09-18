ce059b641f63938180496018efb1344d465942ae
Rudy <mrj9990123@gmail.com>
2018-09-18 14:45:22 +1000

    Load the CSV file, declare, initialize and render the Water, Grass and Tree Tile objects.

--- src/World.java
+++ src/World.java
@@ -1,7 +1,8 @@
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-
+import java.io.*;
+import java.util.ArrayList;
 
 public class World {
 
@@ -38,18 +39,77 @@ public class World {
     // PLAYER_INITIAL_Y is the initial y coordinator of player
     private static final float PLAYER_INITIAL_Y = 720;
 
+    // the reference for level0 source file
+    private static final String LEVEL0_REFERENCE = "assets/levels/0.lvl";
+    // the reference for level1 source file
+    private static final String LEVEL1_REFERENCE = "assets/levels/1.lvl";
+    // the number of values of an Tile objects in lvl file
+    private static final int NUM_OF_VALUES_OF_TILE = 3;
+    // the number of values of an Vehicle objects in lvl file
+    private static final int NUM_OF_VALUES_OF_VEHICLE = 4;
+    // the INDEX of Object class in lvl File
+    private static final int INDEX_OF_OBJ_CLASS_IN_CSV = 0;
+    // the INDEX of x position in lvl File
+    private static final int INDEX_OF_X = 1;
+    // the INDEX of y position in lvl File
+    private static final int INDEX_OF_Y = 2;
+    // the INDEX of Vehicle moving direction in lvl File
+    private static final int INDEX_OF_DIRECTION = 3;
+
+
     // the csv identifier for water
     private static final String WATER = "water";
+    // the csv identifier for water
+    private static final String GRASS = "grass";
+    // the csv identifier for water
+    private static final String TREE = "tree";
+
 
 
     /*Define Sprites and Background*/
     // declare the player
     private Player player;
+    // The ArrayList of Type Water to store Water Tile in the world
+    private ArrayList<Water> waters = new ArrayList<Water>();
+    // The ArrayList of Type Water to store Water Tile in the world
+    private ArrayList<Grass> grasses = new ArrayList<Grass>();
+    // The ArrayList of Type Water to store Water Tile in the world
+    private ArrayList<Tree> trees = new ArrayList<Tree>();
 
 	public World() throws SlickException {
 
 		// Perform initialisation logic
+        try (BufferedReader br = new BufferedReader(new FileReader(LEVEL0_REFERENCE))){
+            // create an array of string to store each line in the format of String
+            String[] description;
+            // String line to store the raw data
+            String line;
+            while((line = br.readLine() )!= null){
+                // load data into description
+                description = line.split(",");
+                float x = Float.parseFloat(description[INDEX_OF_X]);
+                float y = Float.parseFloat(description[INDEX_OF_Y]);
 
+                // create Tile object, if the description contains 3 values
+                if (description.length == NUM_OF_VALUES_OF_TILE){
+                    // create the Water Tile object
+                    if (description[INDEX_OF_OBJ_CLASS_IN_CSV].equals(WATER)){
+                        waters.add(new Water(WATER_REFERENCE, x, y));
+                    }// create the Grass Tile object
+                    else if (description[INDEX_OF_OBJ_CLASS_IN_CSV].equals(GRASS)){
+                        grasses.add(new Grass(GRASS_REFERENCE, x, y));
+                    }// create the Tree Tile object
+                    if (description[INDEX_OF_OBJ_CLASS_IN_CSV].equals(TREE)){
+                        trees.add(new Tree(TREE_REFERENCE, x, y));
+                    }
+                }// create Vehicle object, if the description contains 4 values
+                else if (description.length == NUM_OF_VALUES_OF_VEHICLE){
+
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
         //initialize the player
@@ -73,9 +133,27 @@ public class World {
 	public void render(Graphics g) {
 		// Draw all of the sprites in the game
 
-        //draw the background
+        //draw the Water Tiles
+        ObjectsRender(waters);
+
+        //draw the Grass Tiles
+        ObjectsRender(grasses);
+
+        //draw the Tree Tiles
+        ObjectsRender(trees);
 
         // draw the player
         player.render();
 	}
+
+	/**This function is to draw all the sprites in the Sprite ArrayList
+     *
+     * @param sprites => The array list of created Sprite objects
+     * */
+	private static <T extends Sprite> void ObjectsRender(ArrayList<T> sprites){
+        // draw all the sprite of type T
+        for (T sprite:sprites){
+            sprite.render();
+        }
+    }
 }
