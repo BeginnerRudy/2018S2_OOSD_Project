fb55301cebe669b10da08cdb88bdf17fb3018d3e
Rudy <mrj9990123@gmail.com>
2018-10-12 14:54:19 +1100

    The comment of the Life class.

--- src/Life.java
+++ src/Life.java
@@ -1,12 +1,22 @@
-import org.newdawn.slick.SlickException;
-
+/**
+ * Class: public class Life extends Sprite
+ *
+ * Description: This Life class represents the life of the player.
+ *
+ * */
 public class Life extends Sprite{
     public static final float INITIAL_X_POSITION = 24;
     public static final float INITIAL_Y_POSITION = 744;
     public static final float THE_GAP = 32;
-    // define the image reference of lives
+    /** define the image reference of lives*/
     public static final String LIVES_REFERENCE = "assets/lives.png";
-
+    /**
+     * Method signature: public Life(String imageSrc, float x, float y)
+     *
+     * @param x The given x position
+     * @param y The given y position
+     * @param imageSrc The image reference
+     * */
     public Life(String imageSrc, float x, float y){
         super(imageSrc, x, y);
     }
