e2dc346fd06428fc5a066a5f7e205c0a2f4a9bcd
Rudy <mrj9990123@gmail.com>
2018-10-12 14:16:48 +1100

    The comment of the Player class.

--- src/Player.java
+++ src/Player.java
@@ -5,42 +5,71 @@ import utilities.BoundingBox;
 
 import java.util.ArrayList;
 
+/**
+ * Class: public class Player extends Sprite
+ *
+ * Description: This Player class represent the player object in this game.
+ *              It takes the responsibility of update the position of the player
+ *              and all the other attributes of the player.
+ * */
 public class Player extends Sprite{
-    // define the image reference of player
+    /** define the image reference of player*/
     public static final String PLAYER_REFERENCE = "assets/frog.png";
-    // PLAYER_INITIAL_X is the initial x coordinator of player
+    /** PLAYER_INITIAL_X is the initial x coordinator of player*/
     public static final float PLAYER_INITIAL_X = 512;
-    // PLAYER_INITIAL_Y is the initial y coordinator of player
+    /** PLAYER_INITIAL_Y is the initial y coordinator of player*/
     public static final float PLAYER_INITIAL_Y = 720;
+    /** The step size that a player should take for each move*/
     public static final float STEP_SIZE = 48;
-    public static final int INITIAL_NUMBER_OF_LIVES = 3; //The number of lives that player has while starting game
-    public static final int MIN_NUM_OF_LIFE_TO_PLAY = 1; //The minimun number of lives needed to play
+    /** The number of lives that the player should have at the beginning of the game*/
+    public static final int INITIAL_NUMBER_OF_LIVES = 3;
+    /** The minimum number of lives needed to play*/
+    public static final int MIN_NUM_OF_LIFE_TO_PLAY = 0;
+
+    /** The private attributes of player */
+
+    /** The attribute to indicates that whether the player is contacting with a solid sprite*/
     private boolean isContactWithSolidSprite;
+    /** The attribute to indicates that whether the player is in a hole*/
     private boolean isInAHole;
+    /** The attribute to indicates that whether the player is killed*/
     private boolean isKilled;
+    /** The attribute to indicates that whether the game is over*/
     private boolean isGameOver;
+    /** The attribute to indicates that whether the player is riding on rideable sprite*/
     private boolean isRidden;
+    /** The attribute to represent the next step position that the player would make*/
     private Position nextStep;
+    /** The attribute to represent the next step BoundingBox that the player would be*/
     private BoundingBox nextStepBB;
+    /** The attribute to represent the live status of the player*/
     private ArrayList<Life> lives;
 
-    // The constructor of Player
+    /* ********************************* Methods ******************************************/
+    /**
+     * Method signature: public Player()
+     *
+     * Description: This is the constructor for the class Player, it initialize the attribute of the player.
+     * */
     public Player()  {
+        // call the super class's to initialize parts of the attributes
         super(PLAYER_REFERENCE, PLAYER_INITIAL_X, PLAYER_INITIAL_Y);
-        nextStep = new Position(PLAYER_INITIAL_X, PLAYER_INITIAL_Y); // Initialize the next step Position to the start position
-                                        // due to that there is no information about next step as game just started
+        // Initialize the next step Position to the start position,
+        // due to that there is no information about next step as game just started
+        nextStep = new Position(PLAYER_INITIAL_X, PLAYER_INITIAL_Y);
+        // At the start of the game, all the boolean attributes are false.
         isContactWithSolidSprite = false;
         isInAHole = false;
         isKilled = false;
         isGameOver = false;
         isRidden = false;
-
+        // initialize the bb of the next step
         try {
             nextStepBB = new BoundingBox(new Image(PLAYER_REFERENCE), PLAYER_INITIAL_X, PLAYER_INITIAL_Y);
         } catch (SlickException e) {
             e.printStackTrace();
         }
-
+        // Initialize the lives
         lives = new ArrayList<>();
         // Give the player the initial number of lives, that is 3 lives at beginning.
         for (int i = 0; i<INITIAL_NUMBER_OF_LIVES; i++) {
@@ -54,6 +83,7 @@ public class Player extends Sprite{
      * Method signature: public void behaviour(Player player);
      * Description: This is an empty method, since the player has no behaviour on itself.
      *
+     *
      * @param player The Player object to make behaviour on
      * @param delta The milliseconds since last frame is passed. It does not make se here,
      *                         but may be used in future development.
@@ -63,8 +93,11 @@ public class Player extends Sprite{
         // empty, do nothing.
     }
 
-    // overloading the render method here
-    // to draw the player image on the screen
+    /**
+     * Method signature: public void render()
+     *
+     * Description: This method overloading the render method here,  to draw the player image on the screen and its lives.
+     * */
     @Override
     public void render(){
         super.getImage().drawCentered(super.getPosition().getX(),super.getPosition().getY());
@@ -113,7 +146,7 @@ public class Player extends Sprite{
         // update the position and life of player if it is killed
         if (this.isKilled()) {
             if (this.getLives() != null) {
-                if (this.getLives().size() >= Player.MIN_NUM_OF_LIFE_TO_PLAY) {
+                if (this.getLives().size() > Player.MIN_NUM_OF_LIFE_TO_PLAY) {
                     // remove the leftmost life
                     this.getLives().remove(this.getLives().size() - 1);
                     //restart the player's position
@@ -123,6 +156,47 @@ public class Player extends Sprite{
                 }
             }
         }
+    }
+
+    /**
+     * Method Signature:  private Position move(Input input);
+     *
+     * @param input => the input from the keyboard, especially the directional keys
+     * @return The position of the player's next step depending on the input.
+     * Description: This methods return the Position of next step of the player depending on the given input.
+     * */
+    private Position move(Input input){
+        Position nextStep = new Position(super.getPosition()); // start with current position
+
+        // Move the player stepSize up when the Up key is pressed
+        float playerHeadPositionY = super.getPosition().getY()- (float)super.getImage().getHeight()/2; // the y coordinates of player's head
+        // Move the player stepSize down when the Down key is pressed
+        float playerFeetPositionY = super.getPosition().getY() + (float) super.getImage().getHeight()/2; // the y-coordinates of player's feet
+        // Move the player stepSize left when the Left key is pressed
+        float playerLeftHandPositionX = super.getPosition().getX() - (float)super.getImage().getHeight()/2; // the x coordinate of player's left hand
+        // Move the player stepSize right when the Right key is pressed
+        float playerRightHandPositionX = super.getPosition().getX() + (float)super.getImage().getHeight()/2; // the x coordinate of player's right hand
+
+        if (input.isKeyPressed(Input.KEY_UP) && (playerHeadPositionY >= STEP_SIZE)) {
+            // if there are at least one STEP_SIZE distance from player's head to the upper bound of the screen
+            // take a step of STEP_SIZE
+            nextStep.setY(super.getPosition().getY() - STEP_SIZE);
+        }else if (input.isKeyPressed(Input.KEY_DOWN) && (App.SCREEN_HEIGHT - playerFeetPositionY >= STEP_SIZE)) {
+            // if there are at least one STEP_SIZE distance from player's feet to the lower bound of the screen
+            // take a step of STEP_SIZE
+            nextStep.setY(super.getPosition().getY() + STEP_SIZE);
+        }else if (input.isKeyPressed(Input.KEY_LEFT) && (playerLeftHandPositionX >= STEP_SIZE)){
+            // if the distance from player's left hand to the left side of the screen is at least one STEP_SIZE, take
+            // one STEP_SIZE left
+            nextStep.setX(super.getPosition().getX()-STEP_SIZE);
+        }else if (input.isKeyPressed(Input.KEY_RIGHT) && (App.SCREEN_WIDTH - playerRightHandPositionX >= STEP_SIZE)){
+            // if the distance from player's right hand to the right side of the screen is at least one STEP_SIZE, take
+            // one STEP_SIZE right
+            nextStep.setX(super.getPosition().getX() + STEP_SIZE);
+        }else{// if no directional key is pressed, the position of next step should be same as current Position
+            nextStep = super.getPosition();
+        }
+        return nextStep;
     }
 
     /** Method signature: public void setContactWithSolidSprite(boolean isContactWithSolidSprite);
@@ -169,57 +243,17 @@ public class Player extends Sprite{
     /** Method signature: public void setRidden(boolean gameOver)
      *
      * @param ridden The given boolean value to set
-     *
+     * @retu
      * Description: The setter for the attribute isRidden
      * */
     public void setRidden(boolean ridden) {
         isRidden = ridden;
     }
 
-    /**
-     * Method Signature:  private Position move(Input input);
-     *
-     * @param input => the input from the keyboard, especially the directional keys
-     *
-     * Description: This methods return the Position of next step of the player depending on the given input.
-     * */
-    private Position move(Input input){
-        Position nextStep = new Position(super.getPosition()); // start with current position
-
-        // Move the player stepSize up when the Up key is pressed
-        float playerHeadPositionY = super.getPosition().getY()- (float)super.getImage().getHeight()/2; // the y coordinates of player's head
-        // Move the player stepSize down when the Down key is pressed
-        float playerFeetPositionY = super.getPosition().getY() + (float) super.getImage().getHeight()/2; // the y-coordinates of player's feet
-        // Move the player stepSize left when the Left key is pressed
-        float playerLeftHandPositionX = super.getPosition().getX() - (float)super.getImage().getHeight()/2; // the x coordinate of player's left hand
-        // Move the player stepSize right when the Right key is pressed
-        float playerRightHandPositionX = super.getPosition().getX() + (float)super.getImage().getHeight()/2; // the x coordinate of player's right hand
-
-        if (input.isKeyPressed(Input.KEY_UP) && (playerHeadPositionY >= STEP_SIZE)) {
-            // if there are at least one STEP_SIZE distance from player's head to the upper bound of the screen
-            // take a step of STEP_SIZE
-            nextStep.setY(super.getPosition().getY() - STEP_SIZE);
-        }else if (input.isKeyPressed(Input.KEY_DOWN) && (App.SCREEN_HEIGHT - playerFeetPositionY >= STEP_SIZE)) {
-            // if there are at least one STEP_SIZE distance from player's feet to the lower bound of the screen
-            // take a step of STEP_SIZE
-            nextStep.setY(super.getPosition().getY() + STEP_SIZE);
-        }else if (input.isKeyPressed(Input.KEY_LEFT) && (playerLeftHandPositionX >= STEP_SIZE)){
-            // if the distance from player's left hand to the left side of the screen is at least one STEP_SIZE, take
-            // one STEP_SIZE left
-            nextStep.setX(super.getPosition().getX()-STEP_SIZE);
-        }else if (input.isKeyPressed(Input.KEY_RIGHT) && (App.SCREEN_WIDTH - playerRightHandPositionX >= STEP_SIZE)){
-            // if the distance from player's right hand to the right side of the screen is at least one STEP_SIZE, take
-            // one STEP_SIZE right
-            nextStep.setX(super.getPosition().getX() + STEP_SIZE);
-        }else{// if no directional key is pressed, the position of next step should be same as current Position
-            nextStep = super.getPosition();
-        }
-        return nextStep;
-    }
 
     /** Method signature: public BoundingBox getNextStepBB();
      *
-     * no argument
+     * @return The BoundingBox of next step of the player.
      *
      * Description: The getter of NextStepBB
      * */
@@ -229,7 +263,7 @@ public class Player extends Sprite{
 
     /** Method signature: public Position getNextStep();
      *
-     * no argument
+     * @return The position of the player's next step.
      *
      * Description: The getter of nextPosition
      * */
@@ -239,7 +273,7 @@ public class Player extends Sprite{
 
     /** Method signature: public boolean isContactWithSolidSprite();
      *
-     * no argument
+     * @return The boolean attributes of whether the player is contact with any solid sprites
      *
      * Description: The getter of isContactWithSolidSprite
      * */
@@ -249,7 +283,7 @@ public class Player extends Sprite{
 
     /** Method signature: public boolean isInAHole();
      *
-     * no argument
+     * @return The boolean attributes of whether the player is in a hole.
      *
      * Description: The getter of isInAHole
      * */
@@ -259,7 +293,7 @@ public class Player extends Sprite{
 
     /** Method signature: public boolean isKilled();
      *
-     * no argument
+     * @return The boolean attributes of whether the player is killed.
      *
      * Description: The getter of isKilled
      * */
@@ -269,7 +303,7 @@ public class Player extends Sprite{
 
     /** Method signature: public boolean isGameOver();
      *
-     * no argument
+     * @return The boolean attributes of whether the player is game over.
      *
      * Description: The getter of isGameOver
      *
@@ -281,7 +315,7 @@ public class Player extends Sprite{
 
     /** Method signature: public ArrayList<Life> getLives();
      *
-     * no argument
+     * @return The live ArrayList of the player.
      *
      * Description: The getter of lives
      * */
@@ -291,7 +325,7 @@ public class Player extends Sprite{
 
     /** Method signature: public boolean isRidden();
      *
-     * no argument
+     * @return The boolean attributes of whether the player is riding on any rideablr objects.
      *
      * Description: The getter of isRidden
      * */
