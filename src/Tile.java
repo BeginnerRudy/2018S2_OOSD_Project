0e608225a080167b73617f70c307887f64345c59
Rudy <mrj9990123@gmail.com>
2018-10-12 15:11:02 +1100

    The comment of the Tile class.

--- src/Tile.java
+++ src/Tile.java
@@ -1,15 +1,25 @@
-import org.newdawn.slick.SlickException;
-
-
+/**
+ * Class: public class Tile extends Sprite
+ *
+ * Description: This Tile class represent a Tile objects in the world, that is a water, tree and grass.
+ *              This class is inheriting from the Sprite class.
+ * */
 public class Tile extends Sprite {
-    // define the image reference of grass
+    /** define the image reference of grass*/
     private static final String GRASS_REFERENCE = "assets/grass.png";
 
-    /**
-     * tile size, in pixels
-     */
+    /** tile size, in pixels*/
     public static final float TILE_SIZE = 48;
 
+    /**
+     * Method signature: public Tile(String imgSrc, float x, float y)
+     *
+     * @param x The given x position
+     * @param y The given y position
+     * @param imgSrc The image reference
+     *
+     * Description: This is the constructor of the Tile class.
+     * */
     public Tile(String imgSrc, float x, float y){
         super(imgSrc, x, y);
     }
@@ -19,7 +29,7 @@ public class Tile extends Sprite {
      *
      * @param x The given x-coordinate
      * @param y The given y-coordinate
-     *
+     * @return A grass object.
      * Description: This methods returns a Tile called grass.
      * */
     public static Tile createAGrass(float x, float y){
